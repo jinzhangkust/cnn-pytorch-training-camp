8d896b90ccdea1ac8a99b1aafa32c106079af787
J. Zhang <j.zhang@kust.edu.cn>
2026-01-21 20:14:50 +0800

    Add author information and URL to inception_proj.py

--- models/inception_proj.py
+++ models/inception_proj.py
@@ -1,6 +1,8 @@
 """
 Author: Dr. Jin Zhang
 E-mail: j.zhang@kust.edu.cn
+URL: https://github.com/jinzhangkust
+Dept: Kunming University of Science and Technology (KUST)
 Created on 2024.04.17
 """
 
